3db78cd7b6484575158840f35be796f73587b2de
conconrob <connortprobertson@gmail.com>
2019-02-01 11:29:26 +0000

    INIT commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "quote-props": 0,
+        "quotes": 0,
+        "no-plusplus":0,
+        "no-param-reassign":0,
+        "max-len":0,
+        "no-use-before-define":0,
+    },
+};
